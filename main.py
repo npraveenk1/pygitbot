9323fe66ffcad45b785555f1ae7bd43a521d8d21
praveenn751 <praveenn751@gmail.com>
2021-07-02 01:40:06 -0400

    add sender info

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from flask import Flask, request, Response
+import requests
+
 
 app = Flask(__name__)
 
 @app.route('/webhook', methods=['POST'])
 def respond():
     print(request.json)
+    received_payload = request.json 
+    if received_payload["sender"]["login"] == "npraveenk1":
+        print("Sender is {}".format(received_payload["sender"]["login"]))
     return Response(status=200)
